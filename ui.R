e4fda11ae7419d181672647605c527ffa1a6650c
Daehoon7992 <dpg5313@psu.edu>
2020-07-24 18:11:56 +0900

    Updates on ui.r and server.r
    0. Removed all files in www folder
    
    Ui.r
    1.  Removed unnecessary libraries - shinyjs, formattable and Added 'boastUtils'
    
    2. Reformat the UI in terms of BOAST css style
    
    3. Removed unnecessary css
    
    4. Added arguments on functions
    
    5. Added reference page and references
    
    6. Added Alt text for 'Boxplot' and 'Histogram'
    
    Server.r
    1. Changed the id for readability
    
    2. Removed 'background' argument on renderPlot function, since it causes error - unused argument
    
    3. Streamed lines and removed white spaces.

--- ui.R
+++ ui.R
@@ -1,9 +1,8 @@
 library(shinydashboard)
 library(shiny)
 library(shinyBS)
-library(shinyjs)
 library(shinyWidgets)
-library(formattable)
+library(boastUtils)
 
 sliderInput2 <- function(inputId, label, min, max, value, step=NULL, from_min, from_max){
   x <- sliderInput(inputId, label, min, max, value, step)
@@ -13,155 +12,202 @@ sliderInput2 <- function(inputId, label, min, max, value, step=NULL, from_min, f
              "data-from-shadow" = TRUE)
   x
 }
-ui <- dashboardPage(skin = "yellow",
-                    dashboardHeader(title = "Effect of Outlier",
-                                    tags$li(class="dropdown",
-                                            actionLink("info", icon("info"), class="myClass")),
-                                    tags$li(class='dropdown',
-                                            tags$a(href="https://shinyapps.science.psu.edu/",
-                                                   icon('home', lib='font-awesome')))),
-                    dashboardSidebar(
-                      sidebarMenu(
-                        id="tabs",
-                        #  menuItem("Prerequisite", tabName = "prerequisite3"),
-                        menuItem("Overview", tabName = "overview3",icon = icon("dashboard")),
-                        menuItem("Explore", tabName = "fivenumber", icon = icon("wpexplorer"))
-                      )
-                    ),
-                    dashboardBody(
-                      tags$head(
-                        tags$style(HTML(
-                          '.popover-title{
-                          color: black;
-                          background-color: orange
-                          }'
-                          
-                        )),
-                        
-                        
-                        #Change the location of progress bar
-                        tags$style(
-                          HTML(".shiny-notification {
-                               height: 100px;
-                               width: 800px;
-                               position:fixed;
-                               top: calc(50% - 50px);;
-                               left: calc(50% - 400px);;
-                               }
-                               "
-                          )
-                          )
-                          ),
-                      
-                      tabItems(
-                        # First tab content
-                        tabItem(tabName = "overview3",
-                                #' fluidRow(
-                                # the color for slider bar
-                                tags$link(rel = "stylesheet", type = "text/css", href = "Feature.css"),
-                                tags$style(HTML(".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar {background: orange}")),
-                                tags$style(HTML(".js-irs-1 .irs-single, .js-irs-1 .irs-bar-edge, .js-irs-1 .irs-bar {background: orange}")),
-                                tags$style(HTML(".js-irs-2 .irs-single, .js-irs-2 .irs-bar-edge, .js-irs-2 .irs-bar {background: orange}")),
-                                tags$style(HTML(".js-irs-3 .irs-single, .js-irs-3 .irs-bar-edge, .js-irs-3 .irs-bar {background: orange}")),
-                                
-                                
-                                tags$a(href='http://stat.psu.edu/',   tags$img(src='logo.png', align = "left", width = 180)),
-                                br(),
-                                br(),
-                                br(),
-                                h3(strong("About:")),
-                                h4("In this App, you will observe the effect of outliers on both histograms and boxplots."),
-                                br(),
-                                h3(strong("Instructions:")),
-                                h4(tags$li("Specify  population mean, Standard Deviation and the sample size n.")),
-                                #h4(tags$li("Click GO! to generate a dataset.")),
-                                h4(tags$li("Change the value of the movable point to see how it affects the Histogram & Boxplot.")),
-                                
-                                div(style = "text-align:center",
-                                    bsButton("go", "G O !", icon("bolt"), size = "large", style = "warning", class="circle grow")),
-                                br(),
-                                h3(strong("Acknowledgements:")),
-                                h4("This app was developed and coded by Caihui Xiao and Sitong Liu then further updated by Zhiliang Zhang, Jiajun Gao and Ruisi Wang.")
-                                
-                                
-                                ),
-                        
-                        # Second tab content
-                        tabItem(tabName = "fivenumber",fluidPage(
-                        
-                          wellPanel(
-                            
-                            fluidRow(
-                              column(4,
-                                     sliderInput2("num", "Sample Size:",
-                                                  min = 0, 
-                                                  max = 100, 
-                                                  value = 50, 
-                                                  step = 1, 
-                                                  from_min = 2, 
-                                                  from_max = 100
-                                     )),
-                              
-                              column(4,
-                                     sliderInput("mean",
-                                                 "Population Mean:", 
-                                                 min = -10,
-                                                 max = 10,
-                                                 value = 2)),
-                              
-                              column(4,
-                                     sliderInput("sd", 
-                                                 "Population Standard Deviation:", 
-                                                 min = 0,
-                                                 max = 10, 
-                                                 value = 2))),
-                            
-                            fluidRow(
-                              column(6,
-                                     
-                                     #changed from numeric input to a slider bar 
-                                     
-                                     sliderInput("X1", "Move the outlier (black dot) ",
-                                                 min = -50, 
-                                                 max = 50, 
-                                                 value = 0),
-                                     bsPopover("X1"," ","Drag the slide bar to change the value of black dot on the plot.",
-                                               trigger = "hover", 
-                                               place = "bottom", 
-                                               options = list(container = "body"))
-                              )
-                            )
-                            
-                            
-                          ),
-                          
-                           
-                          wellPanel( 
-                            # div(style = "text-align: center", 
-                            #     h3(strong("Sample Statistics "))),
-                            # h3(tableOutput("values")),
-                            # h3(tableOutput("values2"))),
-                          
-                          
-                          fluidRow(
-                            column(6,
-                                   #br(),
-                                   plotOutput("distPlot2")),
-                            column(6,
-                                   #br(),
-                                   plotOutput("hisPlot2"),
-                                   bsPopover("hisPlot2", "Line info", 
-                                             "Blue line for median and red line for mean.", 
-                                             trigger = "hover", placement = "top"))
-                            
-                          ),
-                          div(style = "text-align: center", 
-                              h3(strong("Sample Statistics "))),
-                          h3(tableOutput("values")),
-                          h3(tableOutput("values2")))
-                          
-                          
-                        )
-                        )
-                        )
-                      ))
+# UI starts ---
+shinyUI(
+  dashboardPage(
+    skin = "yellow",
+    dashboardHeader(
+      titleWidth = 250,
+      title = "Effect of Outlier",
+      tags$li(class="dropdown",
+              actionLink("info", icon("info"), class="myClass")),
+      tags$li(class='dropdown',
+              tags$a(href="https://shinyapps.science.psu.edu/",
+                     icon('home', lib='font-awesome')))),
+    dashboardSidebar(
+      width = 250,
+      sidebarMenu(id="tabs",
+        menuItem("Overview", tabName = "overview",icon = icon("dashboard")),
+        menuItem("Explore", tabName = "explore", icon = icon("wpexplorer")),
+        menuItem("Reference", tabName = "References", icon = icon("leanpub"))
+      ),
+      tags$div(class = "sidebar-logo",
+               boastUtils::psu_eberly_logo("reversed"))
+    ),
+    dashboardBody(
+      tags$head(
+        tags$link(rel = "stylesheet", type = "text/css",
+                  href = "https://educationshinyappteam.github.io/Style_Guide/theme/boast.css")
+        ),
+      tabItems(
+        # First tab content
+        tabItem(
+          tabName = "overview",
+          h1("Effects of Outliers"),
+          #Title
+          p("In this App, you will observe the effect of outliers
+            on both histograms and boxplots."),
+          br(),
+          h2("Instructions"),
+          tags$ol(
+            tags$li("Specify  Population Mean, Standard Deviation,
+                    and the Sample Size n."),
+            tags$li("Change the value of the movable point 
+                    to see how it affects the Histogram & Boxplot.")
+          ),
+          div(
+            style = "text-align:center",
+            bsButton(
+              inputId = "go",
+              label = "GO!",
+              icon("bolt"),
+              size = "large",
+            )
+          ),
+          #Acknowledgements
+          br(),
+          br(),
+          h2("Acknowledgements"),
+          p(
+            "This app was originally developed and coded by Caihui Xiao
+            and Sitong Liu in June 2017. The was further updated by 
+            Zhiliang Zhang and Jiajun Gao in June 2018, 
+            and Ruisi Wang in June 2019, and Daehoon Gwak in July 2020.",
+            br(),
+            br(),
+            br(),
+            div(class = "updated", "Last Update: 7/24/2020 by DG.")
+          )
+        ),
+        # Second tab content
+        tabItem(
+          tabName = "explore",
+          h2('Explore the Effects of Outlier'),
+          fluidRow(
+            column(
+              width = 4,
+              sliderInput2(
+                inputId = "sampleSize",
+                label = "Sample Size:",
+                min = 0,
+                max = 100,
+                value = 50,
+                step = 1,
+                from_min = 2,
+                from_max = 100
+              )
+            ),
+            column(
+              width = 4,
+              sliderInput(
+                inputId = "mean",
+                label = "Population Mean:",
+                min = -10,
+                max = 10,
+                value = 0
+              )
+            ),
+            
+            column(
+              width = 4,
+              sliderInput(
+                inputId = "sd",
+                label = "Population Standard Deviation:",
+                min = 0,
+                max = 10,
+                value = 2
+              )
+            )
+          ),
+          fluidRow(
+            column(
+              width = 6,
+              sliderInput(
+                inputId = "outlier",
+                label = "Move the outlier (black dot)",
+                min = -50,
+                max = 50,
+                value = 0,
+                animate = animationOptions(interval = 200, loop = FALSE)
+              )
+            )
+          ),
+          fluidRow(
+            column(
+              width = 6,
+              plotOutput("boxPlot"),
+              tags$script(HTML(
+                "$(document).ready(function() 
+                       { document.getElementById('boxPlot').
+                       setAttribute('aria-label',
+                       `Shows Boxplot interacting with the sliderInput`)
+                       })"
+              ))
+              ),
+            column(
+              width = 6,
+              plotOutput("hisPlot"),
+              bsPopover(
+                id = "hisPlot",
+                title = "Line info",
+                content = "Blue line for median and red line for mean.",
+                trigger = "hover",
+                placement = "top"
+                ),
+              tags$script(HTML(
+                "$(document).ready(function() 
+                       { document.getElementById('hisPlot').
+                       setAttribute('aria-label',
+                       `Shows Histogram interacting with the sliderInput`)
+                       })"
+              ))
+              )
+            ), 
+          p(strong("Sample Statistics "), align = 'center'),
+          p(tableOutput("values")),
+          p(tableOutput("values2"))
+        ),
+        tabItem(
+          tabName = "References",
+          h2("References"),
+          p(     #shinyBS
+            class = "hangingindent",
+            "Bailey, E. (2015), shinyBS: Twitter bootstrap components for shiny.
+            (v0.61), [R package]. Available from
+            https://CRAN.R-project.org/package=shinyBS"
+          ),
+          p(     #Boast Utilities
+            class = "hangingindent",
+            "Carey, R. (2019), boastUtils: BOAST Utilities. (v0.1.0),
+            [R Package]. Available from
+            https://github.com/EducationShinyAppTeam/boastUtils"
+          ),
+          p(     #shinydashboard
+            class = "hangingindent",
+            "Chang, W. and Borges Ribeio, B. (2018), shinydashboard: Create
+            dashboards with 'Shiny'. (v0.7.1), [R Package]. Available from
+            https://CRAN.R-project.org/package=shinydashboard"
+          ),
+          p(     #shiny
+            class = "hangingindent",
+            "Chang, W., Cheng, J., Allaire, J., Xie, Y., and McPherson, J.
+            (2019), shiny: Web application framework for R. (v1.4.0),
+            [R Package]. Available from https://CRAN.R-project.org/package=shiny"
+          ),
+          p(     #shinyWidgets
+            class = "hangingindent",
+            "Perrier, V., Meyer, F., Granjon, D., Fellows, I., and Davis, W.
+            (2020), shinyWidgets: Custom Inputs Widgets for Shiny
+            (v0.5.2), [R package]. Available from
+            https://cran.r-project.org/web/packages/shinyWidgets/index.html"
+          ),
+          p(     #reference for ideas
+            class = "hangingindent",
+            " Statistical Applets - Mean and Median (n.d.), Available from
+          http://digitalfirst.bfwpub.com/stats_applet/generic_stats_applet_6_meanmed.html"
+          )
+        )
+      )
+    )
+  )
+)
